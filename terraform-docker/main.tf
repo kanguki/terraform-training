6fe05f1d8c8b6962031243e60497444a6079e7a5
mo <kanguki2381@gmail.com>
2023-11-28 06:59:11 +0700

    state changed after running 'terraform import docker_container.alpine_test_import <container-id>', block details are not important

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -14,3 +14,17 @@ provider "docker" {
 resource "docker_image" "nodered" {
   name = "nodered/node-red:latest"
 }
+
+resource "docker_container" "alpine_test_import" {
+    name = "fixed_if_run_terraform_apply_afterwards"
+    image = "alpine:3"
+    ports {
+        internal = "8000"
+        external = "8080"
+    }
+    command = [
+        "tail",
+        "-f",
+        "/dev/null"
+    ]
+}
