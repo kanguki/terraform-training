05686ed4979acad4198450d142d50c5b6a78c86b
mo <kanguki2381@gmail.com>
2023-11-28 07:03:01 +0700

    destroy imported resource by removing resource declaration

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -8,23 +8,9 @@ terraform {
 }
 
 provider "docker" {
-    host = "unix:///Users/monguyen/.colima/docker.sock"
+  host = "unix:///Users/monguyen/.colima/docker.sock"
 }
 
 resource "docker_image" "nodered" {
   name = "nodered/node-red:latest"
-}
-
-resource "docker_container" "alpine_test_import" {
-    name = "fixed_if_run_terraform_apply_afterwards"
-    image = "alpine:3"
-    ports {
-        internal = "8000"
-        external = "8080"
-    }
-    command = [
-        "tail",
-        "-f",
-        "/dev/null"
-    ]
 }
