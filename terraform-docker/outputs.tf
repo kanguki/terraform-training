9d4ff0fd36996e7b293aa2377c75303932a31b8a
mo <kanguki2381@gmail.com>
2023-11-28 15:29:45 +0700

    use for but use join too

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -2,5 +2,5 @@ output nodered_container_whole_names {
     value = docker_container.nodered[*].name
 }
 output nodered_container_external_address {
-    value = [for c in docker_container.nodered[*]: "${c.ports[0].ip}:${c.ports[0].external}"] 
+    value = [for c in docker_container.nodered[*]: join(":", c.ports[*].ip, c.ports[*].external)] 
 }
