1e1d5fbdd84bc205b3f573ded9e102c522fa106c
mo <kanguki2381@gmail.com>
2023-11-28 15:26:47 +0700

    another way

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -2,5 +2,5 @@ output nodered_container_whole_names {
     value = docker_container.nodered[*].name
 }
 output nodered_container_external_address {
-    value = [for k, v in zipmap(docker_container.nodered[*].ports[0].external, docker_container.nodered[*].ports[0].ip): "${k}:${v}"] 
+    value = [for c in docker_container.nodered[*]: "${c.ports[0].ip}:${c.ports[0].external}"] 
 }
