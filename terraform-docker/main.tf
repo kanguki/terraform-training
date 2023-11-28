f7af1879042c35895925e7b7d2b47fb57721a0d0
mo <kanguki2381@gmail.com>
2023-11-28 21:36:36 +0700

    add volume, use depends on

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -10,11 +10,22 @@ resource "random_string" "random" {
   upper   = false
 }
 
+resource "null_resource" "dockervol" {
+  provisioner "local-exec" {
+    command = "mkdir -p noderedvol"
+  }
+}
+
 resource "docker_container" "nodered" {
+  depends_on = [ null_resource.dockervol ]
   count = var.num
   name  = join("-", [lookup(var.container_prefix, var.env), "nodered", random_string.random[count.index].result])
   image = module.image.image_id
   ports {
     internal = "1880"
+  }
+  volumes {
+    container_path = "/data"
+    host_path = "${path.cwd}/noderedvol"
   }
 }
